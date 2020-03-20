0b265ba0f850e31d8b10514d06eba5ef6d1b69b4
Max <maxime.leszczynski@laposte.net>
2020-03-20 14:24:57 +0100

    Remplir le State en un clic

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react'
 // CSS
 import './App.css'
 
+import Header from './components/Header'
+import recettes from './recettes'
+import Admin from './components/Admin'
+
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
   }
+
+  chargerExemple = () => this.setState({ recettes })
 
   render () {
     return (
       <div className='box'>
-        <h1>Bonjour {this.state.pseudo}</h1>
+        <Header pseudo={this.state.pseudo} />
         <div className='cards'>
           <div className='card'>
             <h2>Une Carte</h2>
           </div>
         </div>
+        <Admin chargerExemple={this.chargerExemple} />
       </div>
     )
   }
